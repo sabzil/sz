f4333c39b0ee1274d1cd2c7c99d2b1cf89b91c38
Jaehoon Kim <jaehoon@gmail.com>
2016-04-08 02:02:47 +0900

    crop 옵션 설정

--- vendor/command/crop.go
+++ vendor/command/crop.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mitchellh/cli"
 	"strings"
@@ -12,7 +13,7 @@ type CropCommand struct {
 
 func (c *CropCommand) Help() string {
 	helpText := `
-	Usage: sz crop -src="./foo.jpg" -startx=0 -starty=0 -width=100 -height=20
+	Usage: sz crop -src="./foo.jpg" -x=0 -y=0 -width=100 -height=20
 	`
 	return strings.TrimSpace(helpText)
 }
@@ -23,9 +24,62 @@ func (c *CropCommand) Synopsis() string {
 
 func (c *CropCommand) Run(args []string) int {
 	//fmt.Println("crop run")
-	if len(args) == 0 {
-		fmt.Println(c.Help())
-		return 0
+	var src, x, y, width, height string
+	cmdFlags := flag.NewFlagSet("crop", flag.ContinueOnError)
+	cmdFlags.Usage = func() { c.Ui.Output(c.Help()) }
+	cmdFlags.StringVar(&src, "src", "", "")
+	cmdFlags.StringVar(&x, "x", "", "")
+	cmdFlags.StringVar(&y, "y", "", "")
+	cmdFlags.StringVar(&width, "width", "", "")
+	cmdFlags.StringVar(&height, "height", "", "")
+	if err := cmdFlags.Parse(args); err != nil {
+		return 1
 	}
+
+	if src == "" {
+		c.Ui.Error("원본 사진의 경로를 입력하세요")
+		c.Ui.Error("")
+		c.Ui.Error(c.Help())
+		return 1
+	}
+
+	if x == "" {
+		c.Ui.Error("시작점 x를 입력하세요.")
+		c.Ui.Error("")
+		c.Ui.Error(c.Help())
+		return 1
+	}
+
+	if y == "" {
+		c.Ui.Error("시작점 y를 입력하세요.")
+		c.Ui.Error("")
+		c.Ui.Error(c.Help())
+		return 1
+	}
+
+	if width == "" {
+		c.Ui.Error("자르기 위한 넓이 width를 입력하세요.")
+		c.Ui.Error("")
+		c.Ui.Error(c.Help())
+		return 1
+	}
+
+	if height == "" {
+		c.Ui.Error("자르기 위한 높이 height를 입력하세요.")
+		c.Ui.Error("")
+		c.Ui.Error(c.Help())
+		return 1
+	}
+
+	c.crop(src, x, y, width, height)
+
 	return 0
+}
+
+func (c *CropCommand) crop(src, x, y, width, height string) {
+	fmt.Println(src)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(width)
+	fmt.Println(height)
 }
